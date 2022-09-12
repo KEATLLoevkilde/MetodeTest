e1e73550e3e383c7e2b10d3d4f98a6118b5a1e8f
KEATLLoevkilde <thom4a14@stud.kea.dk>
2022-09-12 10:17:25 +0200

    Tilføjet alder i konstruktør

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -3,12 +3,17 @@ public class Person {
     private double højde;
     private int alder;
 
-    public Person (double højde) {
+    public Person (int alder, double højde) {
         this.højde = højde;
+        this.alder = alder;
     }
 
     public double getHøjde() {
         return højde;
+    }
+
+    public int getAlder() {
+        return alder;
     }
 
     public void setHøjde(double højde) {
