8e9ffe6d02d71edc303654fd68a0c3692ae562d7
KEATLLoevkilde <thom4a14@stud.kea.dk>
2022-09-12 10:10:41 +0200

    Tilføjet get, set og har fødselsdag i Person

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Person p1 = new Person(1.72);
+        System.out.println(p1.getHøjde());
+
+        p1.setHøjde(1.79);
+        System.out.println(p1.getHøjde());
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,8 +1,21 @@
 public class Person {
 
     private double højde;
+    private int alder;
 
     public Person (double højde) {
         this.højde = højde;
+    }
+
+    public double getHøjde() {
+        return højde;
+    }
+
+    public void setHøjde(double højde) {
+        this.højde = højde;
+    }
+
+    public void harFødselsdag() {
+        alder++;
     }
 }
